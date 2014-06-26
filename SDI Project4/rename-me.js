0862889cd1fa6657e12178761ebd0d1c3df9a21e
CurtisT1 <curtist2fullsail.edu>
2014-06-26 18:33:36 -0500

    Changes

--- SDI Project4/rename-me.js
+++ SDI Project4/rename-me.js
@@ -5,14 +5,45 @@ alert("JavaScript works!");
 	SDI 1406
 	Assignment 4 */
 
-
-var phoneNumber = function (matchPattern, phoneNumer){
-if (Pattern === "123,456,7890"){
-	console.log("yay it matches");
-
-} else {
-
-console.log ("it do not match");
+// global Variables 
 
 
+var phoneNumber
+var urlMatch
+var 
+
+
+ phonenumber = function (matchPattern, phoneNumer10){
+	
+	console.log("phoneNumber10" + "123,456,7890."+ "matchPattern." );
 };
+var tenNum = true
+if ( 1234567890 === true) {
+console.log ("Yay it Matches.");
+} else {
+	console.log ("Not a match try again.");
+};
+
+
+
+myPrompt = prompt ("Enter Phone Number");
+
+
+var url = function (typeurl) {
+var preUrl = type.prestr(0,8);
+	if (preUrl.contains ("http://")) {
+return true;
+	} else {
+console.log ("do not Match"); };
+}; 
+
+
+var FirstCase = function (srtinginput) {
+var letter = stringinput.split ("");
+	for (i + 0; i<word.length; i++) { [i].substr(0,1).upperCase() + word[i].substr(1).lowerCase();
+};
+	var capletter = letter.combine ("");
+	return capLetter;
+};
+
+
