1c66859db9e9bd731cd0da288b90cd9c985444ef
CurtisT1 <curtist2fullsail.edu>
2014-06-26 04:03:23 -0500

    Function

--- SDI Project4/rename-me.js
+++ SDI Project4/rename-me.js
@@ -1,1 +1,18 @@
 alert("JavaScript works!");
+
+
+/* Curtis Tate
+	SDI 1406
+	Assignment 4 */
+
+
+var phoneNumber = function (matchPattern, phoneNumer){
+if (Pattern === "123,456,7890"){
+	console.log("yay it matches");
+
+} else {
+
+console.log ("it do not match");
+
+
+};
